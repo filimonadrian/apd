b40975b0b924223da84f2ec6be1c37055df0ada8
filimonadrian <adrian.filimon@hotmail.com>
2020-11-11 11:50:54 +0200

    First exercise solved

--- lab5/oneProducerOneConsumer/Buffer.java
+++ lab5/oneProducerOneConsumer/Buffer.java
@@ -4,13 +4,48 @@ package oneProducerOneConsumer;
  *
  */
 public class Buffer {
-	int a;
+	int a = -1;
+	int size = 0;
 
-	void put(int value) {
+	void produce(int value) {
 		a = value;
+		size = 1;
 	}
 
-	int get() {
-		return a;
+	void consume() {
+		size = 0;
+	}
+
+	boolean isFull() {
+		return size == 1;
+	}
+
+	void put (int value) {
+		synchronized (this) {
+			while (isFull()) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			produce(value);
+			notify();
+		}
+	}
+
+	int get () {
+		synchronized (this) {
+			while (!isFull()) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			consume();
+			notify();
+			return a;
+		}
 	}
 }
