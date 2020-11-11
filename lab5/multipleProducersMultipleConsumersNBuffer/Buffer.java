411091950fcc85952452d7d745980f3bc3c88493
filimonadrian <adrian.filimon@hotmail.com>
2020-11-11 12:03:13 +0200

    Solved second exercise

--- lab5/multipleProducersMultipleConsumersNBuffer/Buffer.java
+++ lab5/multipleProducersMultipleConsumersNBuffer/Buffer.java
@@ -9,16 +9,54 @@ import java.util.Queue;
 public class Buffer {
     
     Queue queue;
-    
+    int size;
+    int currentSize;
+
     public Buffer(int size) {
         queue = new LimitedQueue(size);
+        this.size = size;
+        this.currentSize = 0;
+    }
+    void produce(int value) {
+        queue.add(value);
+        currentSize++;
+    }
+
+    void consume() {
+        currentSize--;
+        // return (int)queue.poll();
+    }
+
+    boolean isFull() {
+        return currentSize == size;
     }
 
 	void put(int value) {
-        queue.add(value);        
+        synchronized (this) {
+            while (isFull()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            produce(value);
+            notify();
+        }
 	}
 
 	int get() {
-        return (int)queue.poll();
+        synchronized (this) {
+            while (!isFull()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            consume();
+            notify();
+            return (int)queue.poll();
+        }
 	}
 }
