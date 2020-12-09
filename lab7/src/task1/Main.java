25847dcb04c66ad5a317889614410b9479adb83d
filimonadrian <adrian.filimon@hotmail.com>
2020-12-09 21:53:10 +0200

    Solved first exercise.

--- lab7/src/task1/Main.java
+++ lab7/src/task1/Main.java
@@ -1,6 +1,9 @@
 package task1;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     static int[][] graph = { { 0, 1 }, { 0, 4 }, { 0, 5 }, { 1, 0 }, { 1, 2 }, { 1, 6 }, { 2, 1 }, { 2, 3 }, { 2, 7 },
@@ -29,8 +32,18 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Integer> partialPath = new ArrayList<>();
-        // se vor calcula toate caile de la nodul 0 la nodul 3 in cadrul grafului
+        AtomicInteger inQueue = new AtomicInteger(0);
+        ExecutorService tpe = Executors.newFixedThreadPool(4);
+        int destination = 3;
         partialPath.add(0);
-        getPath(partialPath, 3);
+
+        // se vor calcula toate caile de la nodul 0 la nodul 3 in cadrul grafului
+        /* serial running */
+//        getPath(partialPath, 3);
+
+        /* parallel running */
+
+        inQueue.incrementAndGet();
+        tpe.submit(new MyRunnable(partialPath, destination, tpe, inQueue));
     }
 }
