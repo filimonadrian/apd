97c3354d92512fba1e8f01f706f483265fdb673d
filimonadrian <adrian.filimon@hotmail.com>
2020-12-17 12:34:00 +0200

    Solved second ex

--- lab8/ex2/ex2.c
+++ lab8/ex2/ex2.c
@@ -12,12 +12,29 @@ int main (int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-    int rand_num;
+    int random_num, ret = 0;
 
+    srand(39);
+
+    if (rank == ROOT) {
+      // generate a number
+      random_num = rand() % 100;
+      // print the generated number
+      printf("Generated number is %d\n", random_num);
+      // send number to all processes
+      ret = MPI_Bcast(&random_num, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+      if (ret != 0) {
+        printf("Process %d can't broadcast\n", rank);
+      }
+    }
+  
     // Root process generates a random number.
     // Broadcasts to all processes.
+    ret = MPI_Bcast(&random_num, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
 
-    printf("Process [%d], after broadcast %d.\n", rank, rand_num);
+    printf("Process [%d], after broadcast %d.\n", rank, random_num);
+
+
 
     MPI_Finalize();
 
